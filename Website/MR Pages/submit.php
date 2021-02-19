3b2136c2cade2ec2453dd9f146862bc81fd7e484
Prathmesh <prathamesh.choudhari18@vit.edu>
2021-02-19 16:50:02 +0530

    Basic submit page added for MR Order

---
+++ Website/MR Pages/submit.php
@@ -0,0 +1,47 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "truventa";
+
+// Create connection
+$connect = new mysqli($servername, $username, $password,$dbname);
+
+if ($connect->connect_error) 
+{
+  echo "Connection failed: ";
+}
+else
+{
+    if(isset($_POST['submit'])){
+        $productArr = $_POST['products'];
+        $qtArr = $_POST['quantities'];
+        $priceArr = $_POST['prices'];
+        if(!empty($productArr)){
+            for($i = 0; $i < count($productArr); $i++){
+                if(!empty($productArr[$i])){
+                    $product = $productArr[$i];
+                    $quantity = $qtArr[$i];
+                    $price  = $priceArr[$i];
+                    
+                    // Database insert query goes here
+                    // $qt=7;
+                    $sql1 = "INSERT INTO `orders` ( `date`, `mrid`, `sid`, `product`, `qty`, `price`, `status`) VALUES (current_timestamp(), '1', '2', '$product', $quantity, $price, 'pending')";
+
+                    mysqli_query($connect,$sql1);
+                    if(mysqli_error($connect))
+                    echo " Record insertion error";
+                    else
+                    echo "<br>Your Order has been successfully placed<br><a href='template.php'>Click here</a>";
+
+
+                }
+            }
+        }
+    }
+}
+
+?>
+
+
