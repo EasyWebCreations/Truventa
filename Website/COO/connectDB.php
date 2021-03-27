02fcc65bdc18f2fc0b1585f315e75bc7b4289a1b
Prathmesh <prathamesh.choudhari18@vit.edu>
2021-03-27 19:36:41 +0530

    1 remaining file added

---
+++ Website/COO/connectDB.php
@@ -0,0 +1,8 @@
+<?php
+
+$connect = mysqli_connect('localhost', 'root', '', 'truventa');
+if (!$connect) {
+    echo "connection error" . mysqli_connect_error();
+} else {
+    // echo "<h3> Database connection success</h3>";
+}
